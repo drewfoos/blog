f8ab116c3805a010084e6ea22bfb95b81bb3b58f
drewfoos <drewfoos@yahoo.com>
2025-01-20 12:54:39 -0500

    updated turnstile

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -255,7 +255,6 @@ export default function ContactPage() {
                       language: "auto",
                       size: "normal",
                       execution: "render",
-                      cData: window.location.hostname,
                     }}
                     onSuccess={(token) => {
                       console.log(
